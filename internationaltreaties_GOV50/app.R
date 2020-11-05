69c5506a2db749b84826526b44c9798edd67bd49
Zelin Liu <liuz@college.harvard.edu>
2020-11-04 22:25:54 -0500

    Milestone #6 commit

--- internationaltreaties_GOV50/app.R
+++ internationaltreaties_GOV50/app.R
@@ -15,7 +15,7 @@ library(ggforce)
 library(readr)
 library(priceR)
 
-d <- read_csv("treaties_data_Oct29.csv") %>% 
+d <- read_csv("treaties_data_Nov3.csv") %>% 
     mutate(number = map_dbl(document, ~ case_when(is.na(.) == TRUE ~ 0,
                                               TRUE ~ 1))) %>% 
     group_by(year) %>% 
@@ -24,18 +24,58 @@ d <- read_csv("treaties_data_Oct29.csv") %>%
     mutate(pres_party = str_replace(pres_party, "D", "Democratic")) %>% 
     mutate(pres_party = str_replace(pres_party, "R", "Republican")) %>% 
     mutate(congress_party = str_replace(congress_party, "D", "Democratic")) %>% 
-    mutate(congress_party = str_replace(congress_party, "R", "Republican")) %>% 
+    mutate(congress_party = str_replace(congress_party, "R", "Republican")) %>%
+    mutate(action_type = na_if(action_type, "Not Applicable")) %>%
+    mutate(topic = str_replace(topic, "Extradition and Criminal Assistance",
+                               "Extrad. and Criminal Assistance")) %>%
+    mutate(topic = str_replace(topic, "International Law and Organization",
+                               "Int. Law and Organization")) %>%
+    mutate(topic = str_replace_all(topic,
+                    c("Agriculture" = "Agriculture",
+                      "Arms Control" = "Arms Ctrl",
+                      "Aviation" = "Aviation", 
+                      "Commercial" = "Commerce",
+                      "Consular" = "Consular",
+                      "Dispute Settlement and Arbitration" = "Arbitr",
+                      "Drugs/Illegal Substances" = "Drugs",
+                      "Environment" = "Enviro",
+                      "Extradition" = "Extradition",
+                      "Extrad. and Criminal Assistance" = "Extradition",
+                      "Fisheries and Wildlife" = "Fish & Wild",
+                      "Human Rights" = "Hum. Rights",
+                      "Intellectual Property/Copyrights" = "I. Prop",
+                      "International Law" = "Int. Law",
+                      "Int. Law and Organization" = "Int. Law",
+                      "Investment" = "Invest",
+                      "Labor" = "Labor",
+                      "Maritime Boundaries and Claims" = "Maritime",
+                      "Mutual Legal Assistance" = "MLAT",
+                      "Shipping and Marine Pollution" = "Maritime",
+                      "Taxation" = "Tax",
+                      "Telecommunications" = "Telecom",
+                      "Terrorism" = "Terror",
+                      "Trademarks/Patents" = "I. Prop",
+                      "United Nations" = "UN"))) %>% 
+    mutate(congress = factor(congress)) %>% 
+    mutate(pres_congress = 
+               case_when(pres_party == "Democratic" & 
+                             congress_party == "Democratic" ~ "Both Dem.",
+                         pres_party == "Republican" & 
+                             congress_party == "Republican" ~ "Both Rep.",
+                         pres_party == "Democratic" & 
+                             congress_party == "Republican" ~ 
+                             "Dem. President \n Rep. Senate",
+                         TRUE ~ "Rep. President \n Dem. Senate")) %>% 
     rename("Year" = year, "President Party" = pres_party, 
            "President" = president, "Congress" = congress, 
            "Senate Party" = congress_party, "Treaty Topic" = topic,
            "Senate Action" = senate_action, 
            "Nat. Defense Spending ($ in Mil.)" = new_dollars,
-           "Number of Treaties in the Year" = number)
+           "Number of Treaties in the Year" = number,
+           "President and Senate Political Parties" = pres_congress)
 
-columns_x <- c("President", "President Party", "Congress", "Senate Party")
-
-columns_y <- c("President", "Treaty Topic", "President Party", "Congress",
-               "Senate Party")
+columns <- c("President", "Treaty Topic", "President Party", "Congress",
+               "Senate Party", "President and Senate Political Parties")
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
@@ -69,12 +109,16 @@ ui <- navbarPage(
              fluidPage(
                  plotOutput("plot1")
                  ),
+             p("What we see here is that there seems to be a negative 
+               correlation between national defense spending and the number of 
+               treaties ratified by the Senate."),
              h3("Make Your Own Graphic"),
+             p(""),
              fluidPage(
-                 selectInput("x", "X variable", columns_x),
-                 selectInput("y", "Y variable", columns_y),
+                 selectInput("x", "X variable", columns),
+                 selectInput("y", "Y variable", columns),
                  selectInput("geom", "geom", c("point", "column", 
-                                               "jitter", "line")),
+                                               "jitter", "bar")),
                  plotOutput("plot2")
                  )
              )
@@ -84,21 +128,21 @@ ui <- navbarPage(
 server <- function(input, output, session) {
     plot_geom <- reactive({
         switch(input$geom,
-               point = geom_point(aes(y = .data[[input$y]])),
+               point = geom_point(aes(y = fct_reorder(.data[[input$y]],
+                                                      .data[["Year"]]))),
                column = 
-                   geom_col(
-                       aes(y = .data[[input$y]],
-                           alpha = d %>%
-                               mutate(number = map_dbl(action_type, 
-                                       ~ case_when(. == "Not Applicable" ~ 0,
-                                                   TRUE ~ 1))) %>%
-                               group_by(input$y) %>%
-                               mutate(number_2 = sum(number)) %>% 
-                               pull(number_2)),
-                                 position = "dodge"),
-               jitter = geom_jitter(aes(y = .data[[input$y]]), width = 0.2, 
+                   geom_col(aes(y = fct_reorder(.data[[input$y]], 
+                                                .data[["Year"]]),
+                                alpha = subset(d, !is.na(action_type)) %>% 
+                                    group_by(input$y) %>% 
+                                    mutate(number = n()) %>% 
+                                    pull(number)),
+                            position = "dodge"),
+               jitter = geom_jitter(aes(y = fct_reorder(.data[[input$y]], 
+                                                        .data[["Year"]])), 
+                                    width = 0.2, 
                                     height = 0.2, alpha = 0.5),
-               line = geom_line(aes(y = .data[[input$y]]))
+               bar = geom_bar()
         )
     })
 
@@ -119,14 +163,18 @@ server <- function(input, output, session) {
     }, res = 96)
     
     output$plot2 <- renderPlot({
-        ggplot(d, aes(.data[[input$x]], 
-                      color = action_type, fill = action_type)) +
+        ggplot(data = subset(d, !is.na(action_type)), 
+               aes(fct_reorder(.data[[input$x]], .data[["Year"]]),
+                   color = action_type, fill = action_type)) +
             plot_geom() +
-            scale_fill_discrete(name = "Senate Action") +
-            scale_color_discrete(name = "Senate Action") +
+            scale_fill_discrete(name = "Senate Action Taken") +
+            scale_color_discrete(name = "Senate Action Taken") +
             scale_alpha_continuous(name = "Number of Treaties") +
-            theme(axis.text.x = element_text(size = 7.5),
-                  axis.text.y = element_text(size = 7.5))
+            theme_bw() +
+            theme(axis.text.x = element_text(size = 7.5, angle = -90),
+                  axis.text.y = element_text(size = 7.5)) +
+            labs(x = input$x,
+                 y = ifelse(input$geom == "bar", "Count", input$y))
     }, res = 96)
 }
 
